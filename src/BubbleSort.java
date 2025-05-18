93f15de1cc1c1bff570d2301784fd621451970e1
RohanT-45 <rohanrthorat@gmail.com>
2025-05-18 20:51:46 +0530

    Add Inital Files

---
+++ src/BubbleSort.java
@@ -0,0 +1,24 @@
+public class BubbleSort {
+    public static void main(String[] args) {
+
+        int[] numbers = {5, 1, 6, 3, 4};
+
+        // Bubble sort logic
+        for (int i = 0; i < numbers.length - 1; i++) {
+            for (int j = 0; j < numbers.length - 1 - i; j++) {
+                if (numbers[j] > numbers[j + 1]) {
+                    // Swap the elements
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+
+        // Printing the sorted array
+        System.out.println("Sorted array in ascending order:");
+        for (int num : numbers) {
+            System.out.print(num + ",");
+        }
+    }
+}
